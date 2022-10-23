60a8e828ed0685e28c4f0e1697e95eaed380d9dd
NAJIB-B <najibajibasu@gmail.com>
2022-10-23 21:12:28 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import profile__img from "./assets/profile__img.png";
+import "./App.css";
 
 function App() {
+  const links = [
+    { name: "Zuri Team", link: "https://training.zuri.team/", id: "btn__zuri" },
+    {
+      name: "Zuri Books",
+      link: "http://books.zuri.team",
+      id: "books",
+    },
+    {
+      name: "Python Books",
+      link: "https://books.zuri.team/python-for-beginners?ref_id=najibbala",
+      id: "book__python",
+    },
+    {
+      name: "Background check for coders",
+      link: "https://background.zuri.team",
+      id: "pitch",
+    },
+    {
+      name: "Design Books",
+      link: "https://books.zuri.team/design-rules",
+      id: "book__design",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main_div">
+      <div className="profile_pic_div">
+        <img
+          src={profile__img}
+          alt="how I look"
+          id="profile__img"
+          className="profile_pic"
+        ></img>
+        <p className="profile_name">Muhmmad Najib Bala</p>
+      </div>
+
+      <p className="username " id="twitter">
+        najib2557
+      </p>
+
+      <p className="username hidden" id="slack">
+        najibbala
+      </p>
+
+      {links.map((item, index) => {
+        return (
+          <a
+            href={item.link}
+            target="_blank"
+            rel="noreferrer"
+            className="link"
+            id={item.id}
+            key={index}
+          >
+            {item.name}
+          </a>
+        );
+      })}
     </div>
   );
 }
